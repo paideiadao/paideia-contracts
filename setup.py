32c9d62b2fbaf4ff031b2c81016abc31e9e0487b
Luivatra <luivatra@gmail.com>
2022-05-12 08:31:38 +0200

    change in setup.py

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/ergo-pad/paideia-contracts",
     include_package_data=True,
+    package_data={'ergo_python_appkit': ['ergo_python_appkit']},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
