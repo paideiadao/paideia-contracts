a7267f9d0309595dbf526f7b1a92a818f3d5df98
Luivatra <luivatra@gmail.com>
2022-05-17 09:36:43 +0200

    version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def glob_fix(package_name, glob):
 
 setuptools.setup(
     name='paideia_contracts',  
-    version='0.0.2',
+    version='0.0.3',
     my_packages=['paideia_contracts'],
     author="Robert Pieter van Leeuwen",
     author_email="luivatra@gmail.com",
