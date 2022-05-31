0a6ed603091aee821524be778382776f10fc449d
Robert Pieter van Leeuwen <luivatra@gmail.com>
2022-05-31 16:12:08 +0200

    emission test case

--- test/test_staking.py
+++ test/test_staking.py
@@ -59,6 +59,92 @@ class TestStaking:
             traceback.print_exc()
         assert signed
 
+    def test_emit_empty(self):
+        stakeStateInput = StakeStateBox(
+            appKit=self.appKit,
+            stakeStateContract=self.config.stakeStateContract,
+            checkpoint=1,
+            checkpointTime=int(self.appKit.preHeader().getTimestamp() - 86400001),
+            amountStaked=10000000,
+            cycleDuration=86400000,
+            stakers=10
+            ).inputBox()
+        stakePoolInput = StakePoolBox(
+            appKit=self.appKit,
+            stakePoolContract=self.config.stakePoolContract,
+            emissionAmount=293000,
+            remaining=900000000
+        ).inputBox()
+        emissionInput = EmissionBox(
+            appKit=self.appKit,
+            emissionContract=self.config.emissionContract,
+            emissionRemaining=0,
+            amountStaked=9569595,
+            checkpoint=0,
+            stakers=0,
+            emissionAmount=293000
+        ).inputBox()
+        stakingIncentiveInput = StakingIncentiveBox(
+            appKit=self.appKit,
+            stakingIncentiveContract=self.config.stakingIncentiveContract,
+            value=int(1e9)
+        ).inputBox()
+        signed = False
+        try:
+            unsignedTx = EmitTransaction(
+                stakeStateInput=stakeStateInput,
+                stakePoolInput=stakePoolInput,
+                emissionInput=emissionInput,
+                stakingIncentiveInput=stakingIncentiveInput,
+                stakingConfig=self.config,
+                address=self.txOperator
+            ).unsignedTx
+            print(ErgoAppKit.unsignedTxToJson(unsignedTx))
+            self.appKit.signTransaction(unsignedTx)
+            signed = True
+        except:
+            traceback.print_exc()
+        assert signed
+
+    def test_compound_single(self):
+        emissionInput = EmissionBox(
+            appKit=self.appKit,
+            emissionContract=self.config.emissionContract,
+            emissionRemaining=271161000,
+            amountStaked=37619804,
+            checkpoint=126,
+            stakers=2,
+            emissionAmount=271161000
+        ).inputBox()
+        stakeBox1 = StakeBox(
+            appKit=self.appKit,
+            stakeContract=self.config.stakeContract,
+            checkpoint=126,
+            stakeTime=0,
+            amountStaked=37619804,
+            stakeKey=self.config.stakedTokenId #this value doesnt matter here
+        ).inputBox()
+        stakingIncentiveInput = StakingIncentiveBox(
+            appKit=self.appKit,
+            stakingIncentiveContract=self.config.stakingIncentiveContract,
+            value=int(4953000000)
+        ).inputBox()
+        signed = False
+        try:
+            unsignedTx = CompoundTransaction(
+                emissionInput=emissionInput,
+                stakeInputs=[stakeBox1],
+                stakingIncentiveInput=stakingIncentiveInput,
+                stakingConfig=self.config,
+                address=self.txOperator
+            ).unsignedTx
+            print(ErgoAppKit.unsignedTxToJson(unsignedTx))
+            self.appKit.signTransaction(unsignedTx)
+            signed = True
+        except:
+            traceback.print_exc()
+        assert signed
+
     def test_compound(self):
         emissionInput = EmissionBox(
             appKit=self.appKit,
